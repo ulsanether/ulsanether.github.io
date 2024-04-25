a829c1cbcd8c578dab497d98870ca5a3c0921b2a
lee hakmin <hangmini12@naver.com>
2024-04-25 13:54:44 +0900

    수정

--- vis-timeline/test/package.test.js
+++
@@ -1,9 +0,0 @@
-import snapshot from "snap-shot-it";
-import { inspectNpmPack } from "vis-dev-utils";
-
-describe("Package", function () {
-  it("Exported files", function () {
-    this.timeout("5m");
-    snapshot(inspectNpmPack());
-  });
-});
